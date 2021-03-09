e79cb05dd560c12bc2dff4c59a5e978c1b1b024c
mutahir <mutahirh@gmail.com>
2021-03-09 13:16:05 -0500

    Added Log4j dependency

--- src/main/java/com/mutahir/training/Application.java
+++ src/main/java/com/mutahir/training/Application.java
@@ -1,14 +1,20 @@
 package com.mutahir.training;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Application {
     
     public Application() {
-        System.out.println ("Inside Application");
+    	final Logger LOGGER = LogManager.getLogger();
+        LOGGER.debug("Inside Application");
+ 
+        
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
+    	//System.out.println ("Starting Application");
 	Application app = new Application();
     }
 }
